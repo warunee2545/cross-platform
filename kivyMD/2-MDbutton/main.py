e3c8e43947bfccf812b7251101d1b8269e8a5f11
warunee2545 <366347221017-st@rmutsb.ac.th>
2024-05-23 15:55:06 +0700

    Add files via upload

---
+++ kivyMD/2-MDbutton/main.py
@@ -0,0 +1,12 @@
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+
+class UI(MDBoxLayout):
+    pass
+
+class DemoApp(MDApp):
+        def build(self):
+              return UI()
+        
+if __name__=="__main__":
+      DemoApp().run()
